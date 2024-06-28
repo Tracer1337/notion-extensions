07e458ce14e00f01631fcae15dac814ada3afbf3
Tracer1337 <merlin.moelter@gmail.com>
2024-06-28 19:24:26 +0200

    Remove white background color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,29 @@
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
 import type { AppProps } from 'next/app'
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: 'transparent',
+        },
+      },
+    },
+  },
+})
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
 
   return (
     <AppCacheProvider {...props}>
-      <CssBaseline>
-        <Component {...pageProps} />
-      </CssBaseline>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Component {...pageProps} />
+        </CssBaseline>
+      </ThemeProvider>
     </AppCacheProvider>
   )
 }
